c4b8cf8491eeadfb6f310886a466d3574615eb64
Karim Alibhai <karim@alibhai.co>
2019-08-08 19:55:52 -0400

    Add: istanbul ignore for module

--- src/module.js
+++ src/module.js
@@ -2,6 +2,7 @@
  * @file src/module.js
  * @copyright Karim Alibhai. All rights reserved.
  */
+/* istanbul ignore file */
 
 export { createQueue, JobPriority } from './queue'
 export { WorkerHandle as Worker } from './worker'
